ab0318e3a414d932cb323aa3029159197eccff12
osakuraei <osakuraei@gmail.com>
2013-10-24 16:29:23 +0800

    check_expression

--- home/db2inst1/tools/check_expression_clone_status.pl
+++ home/db2inst1/tools/check_expression_clone_status.pl
@@ -1,15 +1,14 @@
 #!/opt/ActivePerl-5.16/bin/perl
-
   use strict;
   use DBI();
-if (@ARGV!=2)
+if (@ARGV!=3)
 {
-    die "please check the parameters <primer_id_file> <outputfile>";
+    die "please check the parameters <primer_id_file> <Clone_Type> <outputfile>";
 }
 
 open(PRIMER_ID,"<", $ARGV[0]) or die "PLEASE CHECK IF THERE IS primer_id_file File";
-
-open(RESULT,">>",$ARGV[1]) or die "PLEASE CHECK IF THERE IS AN OUTPUTFILE";
+open(RESULT,">>",$ARGV[2]) or die "PLEASE CHECK IF THERE IS AN OUTPUTFILE";
+my $clone_type=uc($ARGV[1]);
 
   # Connect to the database.
 
@@ -19,23 +18,23 @@ my $dbh = DBI->connect("DBI:mysql:database=lims;host=magic.fulengen.net",
 while (my $primer_id=<PRIMER_ID>)
 {
   chomp($primer_id);
-  my $sth = $dbh->prepare("select sn,primer_id,plate_well,stock_place,vector from _cs_transfer_result where (vector like '%lx304%' or vector like '%Lv122%')  and primer_id=\'$primer_id\'");
+  my $sth = $dbh->prepare("select sn,primer_id,plasmid_place,plate_well,stock_place,vector from _cs_transfer_result where (vector like '%$clone_type%')and primer_id=\'$primer_id\' limit 1");
   $sth->execute();
   my $ref;
   my $numRows = $sth->rows;
   if($numRows>0)
   {
 	  while ($ref = $sth->fetchrow_hashref())
-	    {
-	        print RESULT $ref->{'primer_id'},"\t",$ref->{'sn'},"\t",$ref->{'plate_well'},"\t",$ref->{'stock_place'},"\t",$ref->{'vector'},"\n";
+	    {   
+	        print RESULT $ref->{'primer_id'},"\t",$ref->{'sn'},"\t",$ref->{'plasmid_place'},"\t",$ref->{'plate_well'},"\t",$ref->{'stock_place'},"\t",$ref->{'vector'},"\n";
 	    }
   }
-  else
-    {
+   else
+   {
         
-        	print RESULT $primer_id,"\t","-","\t","-","\t","-","\t","-","\n";
+        	print RESULT $primer_id,"\t","-","\t","-","\t","-","\t","-","\t","-","\n";
         
-    }
+   }
     
   $sth->finish();
 }
