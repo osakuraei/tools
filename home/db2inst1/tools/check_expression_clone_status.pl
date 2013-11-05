19ac75361b6b1537b5c352af2af6fc2ab13e49c5
osakuraei <osakuraei@gmail.com>
2013-11-05 10:52:30 +0800

    check_expression_clone_status.pl

--- home/db2inst1/tools/check_expression_clone_status.pl
+++ home/db2inst1/tools/check_expression_clone_status.pl
@@ -3,7 +3,7 @@
   use DBI();
 if (@ARGV!=3)
 {
-    die "please check the parameters <primer_id_file> <Vector> <outputfile>";
+    die "please check the parameters <primer_id_file/GC_ID_FILE> <Vector> <outputfile>";
 }
 
 open(PRIMER_ID,"<", $ARGV[0]) or die "PLEASE CHECK IF THERE IS primer_id_file File";
@@ -19,7 +19,16 @@ my $dbh = DBI->connect("DBI:mysql:database=lims;host=magic.fulengen.net",
     while (my $primer_id=<PRIMER_ID>)
     {
       chomp($primer_id);
-      my $sth = $dbh->prepare("select sn,primer_id,plasmid_place,plate_well,stock_place,vector from _cs_transfer_result where (vector like '%$clone_type%')and primer_id=\'$primer_id\'");
+      my $sth ="";
+      if ($primer_id=~/^GC-/i)
+      {
+	$sth = $dbh->prepare("select b.gene_id,a.sn,a.primer_id,a.plasmid_place,a.plate_well,a.stock_place,a.vector from _cs_transfer_result a,_glz_design_primer b where (a.vector like '%$clone_type%')and a.primer_id=b.primerid and b.gene_id=\'$primer_id\' limit 1");
+      }
+      
+      else
+      {
+	$sth = $dbh->prepare("select b.gene_id,a.sn,a.primer_id,a.plasmid_place,a.plate_well,a.stock_place,a.vector from _cs_transfer_result a,_glz_design_primer b where (vector like '%$clone_type%')and a.primer_id=\'$primer_id\'and a.primer_id=b.primerid limit 1");
+      }
       $sth->execute();
       my $ref;
       my $numRows = $sth->rows;
@@ -27,13 +36,13 @@ my $dbh = DBI->connect("DBI:mysql:database=lims;host=magic.fulengen.net",
       {
 	      while ($ref = $sth->fetchrow_hashref())
 		{   
-		    print RESULT $ref->{'primer_id'},"\t",$ref->{'sn'},"\t",$ref->{'plasmid_place'},"\t",$ref->{'plate_well'},"\t",$ref->{'stock_place'},"\t",$ref->{'vector'},"\n";
+		    print RESULT $ref->{'gene_id'},"\t",$ref->{'primer_id'},"\t",$ref->{'sn'},"\t",$ref->{'plasmid_place'},"\t",$ref->{'plate_well'},"\t",$ref->{'stock_place'},"\t",$ref->{'vector'},"\n";
 		}
       }
        else
        {
 	    
-		    print RESULT $primer_id,"\t","-","\t","-","\t","-","\t","-","\t","-","\n";
+		    print RESULT $primer_id,"\t","-","\t","-","\t","-","\t","-","\t","-","\t","-","\n";
 	    
        }
 	
