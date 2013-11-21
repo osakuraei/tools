fcbe6d350ab69bbaf15a5f714d8b516fbfef308c
osakuraei <osakuraei@gmail.com>
2013-11-21 17:18:33 +0800

    refine

--- home/db2inst1/tools/get_gene_id_from_symbol.pl
+++ home/db2inst1/tools/get_gene_id_from_symbol.pl
@@ -9,32 +9,33 @@ open(SYM_IN_FILE,"<",$ARGV[0]) or die "please check your input file\n";
 open(CONFIG_IN_FILE,"<","/home/db2inst1/tools/symbol_gene_id_config") or die "please check your CONFIG file\n";
 open(OUT_FILE,">",$ARGV[1]) or die "please check your output file\n";
 my %hash=();
-while (my $line=<CONFIG_IN_FILE>) {
-	chomp($line);
-	my @arr=split /	/,$line;
-	my @new_arr=grep{$_ ne ""}@arr;
-	foreach my $field (1..$#new_arr)
-		{
-				my $FIELD=uc($new_arr[$field]);
-				$hash{$FIELD}=$new_arr[0];
-		}
-	
-}
-
-
-while(my $line=<SYM_IN_FILE>)
+while(my $symbol=<SYM_IN_FILE>)
 {
-	chomp($line);
-	my $LINE=uc($line);
-	if(exists $hash{$LINE})
+	chomp($symbol);
+	my $temp_symbol=uc($symbol);
+	my $flag=1;
+	while (my $line=<CONFIG_IN_FILE>)
 	{
-		my $gene_id=$hash{$LINE};
-		print OUT_FILE $line,"\t",$gene_id,"\n";
+		chomp($line);
+		my @arr=split /	/,$line;
+		my @new_arr=grep{$_ ne ""}@arr;
+		foreach my $field (1..$#new_arr)
+			{
+					my $FIELD=uc($new_arr[$field]);
+					if ($temp_symbol eq  $FIELD)
+					{
+						print OUT_FILE $temp_symbol,"\t",$new_arr[0],"\n";
+						$flag=0;
+					}					
+			}
+		
 	}
-	else
+	seek CONFIG_IN_FILE,0,0;
+	if ($flag==1)
 	{
-		print OUT_FILE $line,"\t","-","\n";
+		print OUT_FILE $temp_symbol,"\t","-","\n";
 	}
+	
 }
 
 close SYM_IN_FILE;
